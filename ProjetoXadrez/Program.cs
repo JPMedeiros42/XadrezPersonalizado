441398ee1cac0ae30edde14ad2c4ecf7377e5c65
João Pedro <medeirosjpro@hotmail.com>
2022-09-30 14:33:14 -0300

    Inicio de um sonho

---
+++ ProjetoXadrez/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Mesa;
+using PartidaX;
+using Enum;
+using ProjetoXadrez.PecasDoTabuleiro;
+
+namespace ProjetoXadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Tabuleiro Tab = new Tabuleiro(8, 8);
+
+                Tab.ColocarPeca(new Rei(Tab, Cor.Preta), new Posicao(0, 0));
+                Tab.ColocarPeca(new Torre(Tab, Cor.Branca), new Posicao(1, 1));
+                Tab.ColocarPeca(new Torre(Tab, Cor.Branca), new Posicao(2, 2));
+
+                
+                Tela.ImprimirTabuleiro(Tab);
+            }
+            catch
+            {
+                throw new TabuleiroException(" Erro Encontrado ");
+            }
+
+
+
+
+        }
+    }
+}
